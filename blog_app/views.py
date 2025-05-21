6e052949a761a088f7352187fb8a98dc352b3bdc
Vahan <mr.revazyan@gmail.com>
2025-05-22 00:34:31 +0400

    add a view function for home page

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Home page view: 
+def index(request): 
+    """Home page for blog."""
+    return render(request, 'blog_app/index.html')
+
