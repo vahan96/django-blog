676bf144232b29df342ffcebe8400229687d8076
Vahan <mr.revazyan@gmail.com>
2025-05-22 00:31:51 +0400

    add url pattern for home page

---
+++ blog_app/urls.py
@@ -0,0 +1,20 @@
+"""Define URL patterns for blog_app."""
+
+from django.urls import path
+from . import views
+# dot means "from current direcory"
+
+app_name = 'blog_app'
+# this helps Django distinguish between several apps in the same project
+
+urlpatterns = [
+    # EXAMPLE: 
+    # path('onboarding', views.onboardgin_view, name='onboarding'),
+    # where urls is example.com/onboarding
+    # call the onboarding_view function
+    # and assign it the name 'onboarding'
+
+    # Home page (empty string): 
+    path('', views.index, name='index'),
+]
+
